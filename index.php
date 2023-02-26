72d894802e35ce30ee0103d93b69a4d4ec018456
bobftool <gonzalezalan321@gmail.com>
2023-02-25 19:20:38 -0600

    Corrección día de la semana, salones próxima hora

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <!-- Esta es la página principal axel-->
+<!-- SET GLOBAL time_zone = '+00:00' -->
 
 <!DOCTYPE html>
 <html lang="es">
@@ -22,6 +23,12 @@
             gtag('config', 'G-V638K0WE9F');
             </script>
         <!-- -->
+        
+        <!-- Color del navegador -->
+        <meta name="theme-color" content="#f16d9f">
+        
+        <!-- Icono homescreen -->
+        <link rel="apple-touch-icon" href="resources\images\design\icono-web_rosa.png">
     </head>
     <body>
         <!--Header-->
@@ -37,7 +44,7 @@
                     </div>
                 </div>
                 <!--Botón-->
-                <a href="#" class="boton">Actualizar</a>
+                <a href="" class="boton">Actualizar</a>
             </div>
         </header>
 
@@ -51,8 +58,8 @@
             <!--Cabecera interior-->
             <div class="cabecera-interior">
                 <div class="cabecera-contenido container">
-                    <h2 class="cabecera-titulo">Salones libres</h2>
-                    <span class="cabecera-subtitulo" id="cabecera-subtitulo">en tiempo real</span>
+                    <h2 class="cabecera-titulo" id="cabecera-track">Hola :)</h2>
+                    <!-- <span class="cabecera-subtitulo" id="cabecera-subtitulo">en tiempo real</span> -->
                 </div>
             </div>
         </section>
@@ -69,125 +76,157 @@
         -->
 
         <!--Grid aulas-->
+        <div class="subtitulo container"> Salones libres ahora:</div>
         <div class="grid-aulas container">
             <?php
-            date_default_timezone_set('America/Mexico_City');
-            $server_time_zone = new DateTimeZone('Europe/London');
-            $local_time_zone = new DateTimeZone('America/Mexico_City');
-            include 'credenciales.php';
+                date_default_timezone_set('America/Mexico_City');
+                $server_time_zone = new DateTimeZone('Europe/London');
+                $local_time_zone = new DateTimeZone('America/Mexico_City');
+                include 'credenciales.php';
 
-            $connection = new mysqli($servername, $username, $password, $database);
+                $connection = new mysqli($servername, $username, $password, $database);
 
-            //revisa la conexión
-            if($connection->connect_error){
-                die("Connection failed: " . $connection->connect_error);
-            }
-
-            //revisar hora actual
-            $sql = "SELECT CURRENT_TIME()";
-            $result =  $connection->query($sql);
-            $row = $result->fetch_assoc();
-            $result_string = $row["CURRENT_TIME()"];
-            $hora_actual = (new DateTime($result_string, $server_time_zone))->setTimezone($local_time_zone);
-
-            //echo $hora_actual->format('Y/m/d H:i:s');
-            
-            //revisar fecha actual
-            $sql = "SELECT CURDATE()";
-            $result =  $connection->query($sql);
-            $row = $result->fetch_assoc();
-            $result_string = $row["CURDATE()"];
-            $fecha_actual = (new DateTime($result_string, $server_time_zone))->setTimezone($local_time_zone);
-
-            //revisar dia de la semana actual
-            $sql = 'SELECT DAYOFWEEK("' . $fecha_actual->format('Y-m-d') . '")';
-            $result =  $connection->query($sql);
-            $row = $result->fetch_assoc();
-            $result_string = $row['DAYOFWEEK("' . $fecha_actual->format('Y-m-d') . '")'];
-            $dia_actual = intval($result_string);
-
-            //obtener todos los horarios
-            $sql = "SELECT * FROM horas";
-            $result =  $connection->query($sql);
-            for($i = 1; $row = $result->fetch_assoc(); $i++){
-                $result_string = $row["hora_inicio"];
-                $horas[$i] = new DateTime($result_string);
-                //echo date_format($horarios[$i], "H:i:s");
-            }
-
-            //obtener todos los dias
-            $sql = "SELECT * FROM dias";
-            $result =  $connection->query($sql);
-            for($i = 1; $row = $result->fetch_assoc(); $i++){
-                $result_string = $row["dias"];
-                $dias[$i] = $result_string;
-            }
-
-            //comparar dia y hora con los horarios
-            //echo $dia_actual . " XD " . "XDXDXD";
-            //echo date('H:i:s', strtotime('10:30:00'));
-            //echo strtotime('10:30:00');
-
-            //obtener id del horario actual de acuerdo a la hora y dia de la semana actual
-            $horario = 0;
-            for($i=1; $i<=sizeof($horas); $i++){
-                if($hora_actual >= $horas[$i] && $hora_actual <= $horas[$i]->modify('+90 minutes')){
-                    $horario=$i+($dia_actual-2)*10;
+                //revisa la conexión
+                if($connection->connect_error){
+                    die("Connection failed: " . $connection->connect_error);
                 }
-            }
+
+                //revisar hora actual
+                $sql = "SELECT CURRENT_TIME()";
+                $result =  $connection->query($sql);
+                $row = $result->fetch_assoc();
+                $result_string = $row["CURRENT_TIME()"];
+                $hora_actual = (new DateTime($result_string, $server_time_zone))->setTimezone($local_time_zone);
+                //$hora_actual = (new DateTime('12:00:00', $local_time_zone));
 
 
-            //Leer datos de la base de datos
-            //$sql = "SELECT * FROM aulas";
-            /*
-            $sql = "SELECT DISTINCT aulas.*
-                    FROM aulas AS aulas
-                    JOIN horarios_aulas AS lista ON aulas.id = lista.id_aulas
-                    JOIN horarios AS horarios ON horarios.id = lista.id_horarios
-                    AND horarios.id = DAYOFWEEK(CURDATE())-1";
-            
-            $dia_actual = DAYOFWEEK(CURDATE());
-            $hora_actual =  intval(HOUR(CURTIME) .  MINUTE(CURTIME));
-            echo $hora_actual;
-            $horario_actual=9;
-
-            if($hora_actual >= 700 and $hora_actual < 830){
+                //echo $hora_actual->format('Y/m/d H:i:s');
                 
-            }*/
+                //revisar fecha actual
+                $sql = "SELECT CURRENT_TIMESTAMP";
+                $result =  $connection->query($sql);
+                $row = $result->fetch_assoc();
+                $result_string = $row["CURRENT_TIMESTAMP"];
+                $fecha_actual = (new DateTime($result_string, $server_time_zone))->setTimezone($local_time_zone);
+                
+                //revisar dia de la semana actual
+                $sql = 'SELECT DAYOFWEEK("' . $fecha_actual->format('Y-m-d') . '")';
+                $result =  $connection->query($sql);
+                $row = $result->fetch_assoc();
+                $result_string = $row['DAYOFWEEK("' . $fecha_actual->format('Y-m-d') . '")'];
+                $dia_actual = intval($result_string);
 
-            /*$sql =
-            "SELECT DISTINCT aulas.*
-            FROM horarios_aulas AS lista
-            JOIN aulas AS aulas ON aulas.id = lista.id_aulas
-            JOIN horarios AS horarios ON horarios.id = lista.id_horarios
-            WHERE lista.id_horarios = " . $horario_actual;
-            */
-            
-            $sql = "SELECT * FROM aulas WHERE id IN (SELECT id_aulas FROM horarios_aulas WHERE id_horarios = ".$horario.")";
-            $result = $connection->query($sql);
+                //obtener todos los horarios
+                $sql = "SELECT * FROM horas";
+                $result =  $connection->query($sql);
+                for($i = 1; $row = $result->fetch_assoc(); $i++){
+                    $result_string = $row["hora_inicio"];
+                    $horas[$i] = new DateTime($result_string, $local_time_zone);
+                    //echo date_format($horarios[$i], "H:i:s");
+                }
 
-            if(!$result){
-                die("Invalid query: " . $connection->error);
-            }
+                //obtener todos los dias
+                $sql = "SELECT * FROM dias";
+                $result =  $connection->query($sql);
+                for($i = 1; $row = $result->fetch_assoc(); $i++){
+                    $result_string = $row["dias"];
+                    $dias[$i] = $result_string;
+                }
 
-            if($horario=0){
-                echo "La esucela está cerrada :(";
-            }
+                //comparar dia y hora con los horarios
+                //echo $dia_actual . " XD " . "XDXDXD";
+                //echo date('H:i:s', strtotime('10:30:00'));
+                //echo strtotime('10:30:00');
 
-            //Mostrar los resultados encontrados
-            while($row = $result->fetch_assoc()){
-                echo
-                "<div class='aula-container'>
-                    <div class='aula-estado'>Libre</div>
-                    <div class='aula-numero'>" . $row["id"] . "</div>
-                    <div class='aula-edificio'>Edificio <span>" . $row["edificio"] . "</span></div>
-                    <div class='aula-piso'>Piso <span>" . $row["piso"] . "</span></div>
-                    <div class='aula-salon'>Salón <span>" . $row["salon"] . "</span></div>
-                </div>";
-            }
+                //obtener id del horario actual de acuerdo a la hora y dia de la semana actual
+                $horario = 0;
+                for($i=1; $i<=sizeof($horas); $i++){
+                    if($hora_actual >= $horas[$i] && $hora_actual <= $horas[$i]->modify('+90 minutes')){
+                        $horario=$i+($dia_actual-2)*10;
+                        break;
+                    }
+                }
+
+
+                //Leer datos de la base de datos
+                //$sql = "SELECT * FROM aulas";
+                /*
+                $sql = "SELECT DISTINCT aulas.*
+                        FROM aulas AS aulas
+                        JOIN horarios_aulas AS lista ON aulas.id = lista.id_aulas
+                        JOIN horarios AS horarios ON horarios.id = lista.id_horarios
+                        AND horarios.id = DAYOFWEEK(CURDATE())-1";
+                
+                $dia_actual = DAYOFWEEK(CURDATE());
+                $hora_actual =  intval(HOUR(CURTIME) .  MINUTE(CURTIME));
+                echo $hora_actual;
+                $horario_actual=9;
+
+                if($hora_actual >= 700 and $hora_actual < 830){
+                    
+                }*/
+
+                /*$sql =
+                "SELECT DISTINCT aulas.*
+                FROM horarios_aulas AS lista
+                JOIN aulas AS aulas ON aulas.id = lista.id_aulas
+                JOIN horarios AS horarios ON horarios.id = lista.id_horarios
+                WHERE lista.id_horarios = " . $horario_actual;
+                */
+                
+                $sql = "SELECT * FROM aulas WHERE id IN (SELECT id_aulas FROM horarios_aulas WHERE id_horarios = ".$horario.")";
+                $result = $connection->query($sql);
+
+                if(!$result){
+                    die("Invalid query: " . $connection->error);
+                }
+
+                if($horario==0){
+                    echo "La esucela está cerrada :(";
+                }
+
+                //Mostrar los resultados encontrados
+                while($row = $result->fetch_assoc()){
+                    echo
+                    "<div class='aula-container'>
+                        <div class='aula-estado'>Libre</div>
+                        <div class='aula-numero'>" . $row["id"] . "</div>
+                        <div class='aula-edificio'>Edificio <span>" . $row["edificio"] . "</span></div>
+                        <div class='aula-piso'>Piso <span>" . $row["piso"] . "</span></div>
+                        <div class='aula-salon'>Salón <span>" . $row["salon"] . "</span></div>
+                    </div>";
+                }
             ?>
         </div>
 
+        <div class="subtitulo container"> Salones libres en la próxima hora:</div>
+        <div class="grid-aulas container">
+            <?php
+                $sql = "SELECT * FROM aulas WHERE id IN (SELECT id_aulas FROM horarios_aulas WHERE id_horarios = ".($horario+1).")";
+                $result = $connection->query($sql);
+
+                if(!$result){
+                    die("Invalid query: " . $connection->error);
+                }
+
+                if($horario==0){
+                    echo "La esucela está cerrada :(";
+                }
+                else{
+                    //Mostrar los resultados encontrados
+                    while($row = $result->fetch_assoc()){
+                        echo
+                        "<div class='aula-container' id='disabled'>
+                            <div class='aula-estado'>[" . $horas[$i]->format('h:i a') . "]</div>
+                            <div class='aula-numero'>" . $row["id"] . "</div>
+                            <div class='aula-edificio'>Edificio <span>" . $row["edificio"] . "</span></div>
+                            <div class='aula-piso'>Piso <span>" . $row["piso"] . "</span></div>
+                            <div class='aula-salon'>Salón <span>" . $row["salon"] . "</span></div>
+                        </div>";
+                    }
+                }
+            ?>
+        </div>
         <!--Conexión con JavaScript-->
         <script src="main.js"></script>
     </body>
